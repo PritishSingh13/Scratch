ac90601b0dfdb2adc296fff74c5e7017fda4e192
pritishsg13@gmail.com <[pritishsg13@gmail.com]>
2025-02-23 01:33:15 +0530

    added separations of concerns in models

---
+++ models/usermodel.js
@@ -0,0 +1,22 @@
+const mongoose = require ('mongoose');
+
+const userSchema = mongoose.Schema({
+    fullname : String,
+    email : String ,
+    password : String , 
+    cart :{
+        type:Array,     //use this method to write 
+        default:[]               //for array part if your type is array
+        
+    },
+    isadmin: Boolean,
+    orders: {
+        type: Array,
+        default:[]
+    },
+    contact : Number, 
+    picture : String,
+
+});
+
+module.exports = mongoose.model("user", userSchema);
