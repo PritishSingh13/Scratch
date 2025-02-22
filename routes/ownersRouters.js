df77b0065956a32cb593c1b46dad711c592c7408
pritishsg13@gmail.com <[pritishsg13@gmail.com]>
2025-02-23 02:59:55 +0530

    added developement env

--- routes/ownersRouters.js
+++ routes/ownersRouters.js
@@ -5,15 +5,34 @@ const ownerModel = require("../models/ownermodel")
 
 
 
+if(process.env.NODE_ENV === "development"){
+    router.post("/create" , async function(req,res){
+    let owners = await ownerModel.find();
+    if(owners.length > 0 ){
+        return res.send(503)
+        res.send("You don't have permision to create a new owner");
+
+    }
+
+    let {fullname, email , password} = req.body;
+
+
+    let createdOwner = await ownerModel.create({
+        fullname,
+        email,
+        password,
+    });
+
+    res.send("createdOwner");
+    });
+}
+
+
+
 router.get("/" , function(req,res){
     res.send("hey its working");
 });
 
-if(process.env.NODE_ENV === "development"){
-    router.post("/create" , function(req,res){
-        res.send("hey its working");
-    });
-}
 
 
 
